e10ab70866179f52323ec03357a9f497bf63fa97
Turtin <104549951+Turtin@users.noreply.github.com>
2024-07-10 01:26:56 +1000

    Start on bot, still trying to get discord to add my bot to my own server

--- src/main/java/net/duckycraft/server_tools/ServerTools.java
+++ src/main/java/net/duckycraft/server_tools/ServerTools.java
@@ -1,14 +1,18 @@
 package net.duckycraft.server_tools;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ServerTools extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        saveDefaultConfig();
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "ServerTools has been enabled!");
     }
 
     @Override
     public void onDisable() {
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "ServerTools has been enabled!");
     }
 }
